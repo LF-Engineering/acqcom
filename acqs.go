bfad5c5e36711455446bef482ba80625aa6f30f3
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-03-09 09:59:05 +0000

    Finished tool - still some manual udpates needed
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- acqs.go
+++ acqs.go
@@ -28,11 +28,11 @@ func initDB() *sqlx.DB {
 	return d
 }
 
-func acqcom(db *sqlx.DB, when string, oid, nid int) {
-	id, uuid, start, end, pSlug, role := 0, "", time.Now(), time.Now(), "", ""
-	rows, err := db.Query("select id, uuid, start, end, project_slug, role from enrollments where organization_id = ? and end > ?", oid, when)
+func acqcom(db *sqlx.DB, when time.Time, oid, nid int) {
+	id, uuid, start, end, pSlug, role, cnt, i, n := 0, "", time.Now(), time.Now(), "", "", 0, 0, 0
+	rows, err := db.Query("select count(id) from enrollments where organization_id = ? and end > ?", oid, when)
 	for rows.Next() {
-		err = rows.Scan(&id, &uuid, &start, &end, &pSlug, &role)
+		err = rows.Scan(&n)
 		if err != nil {
 			return
 		}
@@ -45,6 +45,84 @@ func acqcom(db *sqlx.DB, when string, oid, nid int) {
 	if err != nil {
 		return
 	}
+	fmt.Printf("%d enrollments to process\n", n)
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Printf("error %+v\n", err)
+		return
+	}
+	defer func() {
+		if tx != nil {
+			tx.Rollback()
+		}
+	}()
+	rows, err = db.Query("select id, uuid, start, end, project_slug, role from enrollments where organization_id = ? and end > ?", oid, when)
+	for rows.Next() {
+		err = rows.Scan(&id, &uuid, &start, &end, &pSlug, &role)
+		if err != nil {
+			return
+		}
+		i++
+		if i%25 == 0 {
+			fmt.Printf("%d/%d\n", i, n)
+		}
+		rows2, err := tx.Query("select count(id) from enrollments where organization_id = ? and uuid = ? and project_slug = ? and (start < ? or end > ?)", nid, uuid, pSlug, end, start)
+		//rows2, err := tx.Query("select count(id) from enrollments where organization_id = ? and uuid = ? and project_slug = ?", nid, uuid, pSlug)
+		for rows2.Next() {
+			err = rows2.Scan(&cnt)
+			if err != nil {
+				return
+			}
+		}
+		err = rows2.Err()
+		if err != nil {
+			return
+		}
+		err = rows2.Close()
+		if err != nil {
+			return
+		}
+		if !start.Before(when) {
+			if cnt > 0 {
+				fmt.Printf("enrollment %d needs manual update, its start & end dates are after %v but also has new company's enrollments\n", id, when)
+				continue
+			}
+			_, err = tx.Exec("update enrollments set organization_id = ? where id = ?", nid, id)
+			if err != nil {
+				fmt.Printf("error %+v\n", err)
+				return
+			}
+			continue
+		}
+		if cnt > 0 {
+			fmt.Printf("enrollment %d needs manual update, it intersects %v but also has new company's enrollments 'select * from enrollments where uuid = '%s' and organization_id in (%d, %d) order by start, end'\n", id, when, uuid, oid, nid)
+			continue
+		}
+		_, err = tx.Exec("update enrollments set end = ? where id = ?", when, id)
+		if err != nil {
+			fmt.Printf("error %+v\n", err)
+			return
+		}
+		_, err = tx.Exec("insert into enrollments(uuid, organization_id, start, end, project_slug, role) values(?, ?, ?, ?, ?, ?)", uuid, nid, when, end, pSlug, role)
+		if err != nil {
+			fmt.Printf("error %+v\n", err)
+			return
+		}
+	}
+	err = rows.Err()
+	if err != nil {
+		return
+	}
+	err = rows.Close()
+	if err != nil {
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		fmt.Printf("error %+v\n", err)
+		return
+	}
+	tx = nil
 }
 
 func main() {
@@ -83,6 +161,10 @@ func main() {
 	if oid == 0 || nid == 0 {
 		return
 	}
-	when := os.Getenv("DT")
+	when, err := time.Parse("2006-01-02", os.Getenv("DT"))
+	if err != nil {
+		fmt.Printf("error %+v\n", err)
+		return
+	}
 	acqcom(db, when, oid, nid)
 }
